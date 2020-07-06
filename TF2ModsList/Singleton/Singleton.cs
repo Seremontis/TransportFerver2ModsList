07ad03ed3f12710732aeee3b06e1357c497c68d9
Karol <karols96@interia.pl>
2020-07-06 19:43:06 +0200

    Revert "Revert "#0""
    This reverts commit fcdb1e523033509a4a14a66d0e4f152acfbc876b.

---
+++ TF2ModsList/Singleton/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TF2ModsList.Models
+{
+    //singleton
+    public class Singleton
+    {
+        private static DataApp _instance;
+
+        public static DataApp Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DataApp();
+                return _instance;
+            }
+        }
+    }
+}
