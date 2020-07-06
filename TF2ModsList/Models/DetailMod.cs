865b7fcc6b88be8991fd06bfa92a3b5c77f50021
Karol <karols96@interia.pl>
2020-07-06 19:33:13 +0200

    #0

---
+++ TF2ModsList/Models/DetailMod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TF2ModsList.Models
+{
+    public class DetailMod
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri MainPicture { get; set; }
+        public List<Uri> ListPictures { get; set; }
+        public Dictionary<string, Uri> VersionFiles { get; set; }
+        public string SteamId { get; set; }
+        public List<string> Authors { get; set; }
+        public string SpecificationMod { get; set; }
+        //Odnośnik do strony źródłowej
+        //public string Source { get; set; }
+    }
+}
---
+++ TF2ModsList/Singleton/IDataApp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TF2ModsList.Models
+{
+    interface IDataApp
+    {
+        void DeleteOfHistory(string uri);
+    }
+}
