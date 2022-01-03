672f0fb084309934b9fb8d21a1441b346a86501e
K2\K2 <seremontis1996@gmail.com>
2022-01-03 17:29:14 +0100

    init importer for steam

---
+++ ImporterSteam/ImporterSteam/Logic/SteamWebsiteLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Importer;
+
+namespace ImporterSteam.Logic
+{
+    public class SteamWebsiteLoader
+    {
+        private string _defaultUrl = "https://steamcommunity.com/workshop/browse/?appid=1066780&browsesort=mostrecent&section=readytouseitems&actualsort=mostrecent&";
+        private string _pageParams="p=";
+        private int _maxPageNumber = 1;
+        private List<Mod> _mods = new List<Mod>();
+        private HtmlWeb _web = new HtmlWeb();
+        private HtmlNode _html;
+
+        public void TestConnect()
+        {
+            LoadSite(_maxPageNumber);
+            GetMaxPage();
+            FillListMods();
+            for (int i = 2; i < _maxPageNumber; i++)
+            {
+                LoadSite(i);
+                FillListMods();
+            }
+        }
+
+        private void LoadSite(int number) =>  _html = _web.Load(_defaultUrl + _pageParams + number)?.DocumentNode;
+
+        private void GetMaxPage()
+        {
+            _maxPageNumber = _html.SelectSingleNode("//div[@class='workshopBrowsePagingControls']").ChildNodes.Select(x =>
+                {
+                    int res;
+                    int.TryParse(x.InnerText, out res);
+                    return res;
+                }).Max();
+        }
+
+        private void FillListMods()
+        {
+            var t = _html;
+        }
+    }
+}
---
+++ ImporterSteam/ImporterSteam/Program.cs
@@ -0,0 +1,15 @@
+﻿using ImporterSteam.Logic;
+using System;
+
+namespace ImporterSteam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SteamWebsiteLoader steamWebsiteLoader = new SteamWebsiteLoader();
+            steamWebsiteLoader.TestConnect();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
